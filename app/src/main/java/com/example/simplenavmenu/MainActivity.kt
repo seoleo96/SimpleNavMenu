bfc983b6278a9220561dbfc55d667d989af558bb
Sardor <59287789+seoleo96@users.noreply.github.com>
2021-01-18 13:38:58 +0600

    commit get

--- app/src/main/java/com/example/simplenavmenu/MainActivity.kt
+++ app/src/main/java/com/example/simplenavmenu/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.simplenavmenu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -11,24 +12,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val navMenu = findViewById<BottomNavigationView>(R.id.navMenu)
         navMenu.setOnNavigationItemSelectedListener {
-            when(it.itemId){
-                R.id.first -> {
-                    findViewById<TextView>(R.id.textView).text = "First"
-                    true
-                }
-
-                R.id.second -> {
-                    findViewById<TextView>(R.id.textView).text = "second"
-                    true
-                }
-                R.id.third ->{
-                    findViewById<TextView>(R.id.textView).text = "third"
-                    true
-                }
-                else -> false
-            }
-
+           navClick(it)
         }
 
+
+    }
+
+    private fun navClick(it : MenuItem): Boolean {
+        when(it.itemId){
+            R.id.first -> {
+                findViewById<TextView>(R.id.textView).text = "First"
+                true
+            }
+
+            R.id.second -> {
+                findViewById<TextView>(R.id.textView).text = "second"
+                true
+            }
+            R.id.third ->{
+                findViewById<TextView>(R.id.textView).text = "third"
+                true
+            }
+            else -> false
+        }
+        return false
     }
 }
