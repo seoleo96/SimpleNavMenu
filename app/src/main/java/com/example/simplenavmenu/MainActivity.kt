2eaf45b214c3a8ca1f91ff98f6e739590402f8ac
Sardor <59287789+seoleo96@users.noreply.github.com>
2021-01-18 13:31:10 +0600

    Initial commit

---
+++ app/src/main/java/com/example/simplenavmenu/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.simplenavmenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navMenu = findViewById<BottomNavigationView>(R.id.navMenu)
+        navMenu.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.first -> {
+                    findViewById<TextView>(R.id.textView).text = "First"
+                    true
+                }
+
+                R.id.second -> {
+                    findViewById<TextView>(R.id.textView).text = "second"
+                    true
+                }
+                R.id.third ->{
+                    findViewById<TextView>(R.id.textView).text = "third"
+                    true
+                }
+                else -> false
+            }
+
+        }
+
+    }
+}
